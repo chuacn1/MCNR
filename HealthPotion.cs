9411b09dcc6f503131b7a79485d976a421c45e00
Dipkumar Dhirubhai Kevadiya (1000125807) <KEVAD2@student.op.ac.nz>
2025-02-28 14:27:01 +1300

    change in Healthpotion.cs file

--- HealthPotion.cs
+++ HealthPotion.cs
@@ -1,13 +1,26 @@
-public class HealthPotion
-{
-    //details for health potion
-    public string name { get; set; }
-    public int healingAmount { get; set; }
-    public int Cost { get; set; }
-    public int maxUses { get; set; } = 3;
-    public int currentUses { get; set; }
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-    //required material to craft this potion
-    public static string requiredMaterial = "Delicate Flower";
-    public static int requiredQuantity = 1;
+namespace MCNR
+{
+    public class HealthPotion
+    {
+        //details for health potion
+        public string name { get; set; }
+        public int healingAmount { get; set; }
+        public int Cost { get; set; }
+        public int maxUses { get; set; } = 3;
+        public int currentUses { get; set; }
+
+        //required material to craft this potion
+        public static string requiredMaterial = "Delicate Flower";
+        public static int requiredQuantity = 1;
+    }
 }
+
+
+
+
