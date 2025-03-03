0a4c95edd0dc56d6ef906bc319abf66393d357a0
Clarissa Chua (1000126504) <CHUACN1@student.op.ac.nz>
2025-03-04 12:00:23 +1300

    Doing my stuff halfway, gonna make a new method for C3orRetraceorExit

--- Cave.cs
+++ Cave.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace MCNR
 {
-    internal class Cave
+    public class Cave
     {
+        public static void Cave1or2()
+        {
+            string C1C2choice = Narration.C1orC2();
+
+            if (C1C2choice == "1")
+            {
+                string GloomBeastChoice = Narration.C1GloomBeast();
+                if (GloomBeastChoice == "1")
+                {
+                    //EnemyVSPlayer
+                }
+                else 
+                { 
+                    //Flee Narration
+                }
+            }
+            if (C1C2choice == "2")
+            {
+                string GemStoneChoice = Narration.C3GemstoneTitan();
+                if (GemStoneChoice == "1")
+                {
+                    //EnemyVSPlayer
+                    Narration.C3orRetraceorExit(); //put in method
+                }
+                else
+                {
+                    //Flee Narration
+                }
+            }
+            if (C1C2choice == "0")
+            {
+                Narration.ExitingCave();
+                Narration.VeldrosDialogue(); //PlayerName, Veldros.cs
+                Narration.PickRoute(); //PlayerName, Veldros.cs 
+            }
+        }
     }
 }
