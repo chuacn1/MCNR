621138e9e18603a1ca3219e94c85d3ac27c4bbee
Clarissa Chua (1000126504) <CHUACN1@student.op.ac.nz>
2025-03-06 19:10:23 +1300

    added loops, changed the format of how it receives the input, use all switch instead of if else, continuation of escapecave needed

--- Cave.cs
+++ Cave.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.Arm;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,45 +10,61 @@ namespace MCNR
     public class Cave
     {
         public static void Cave1Or2()
-        { //loop
-            int C1C2choice = Narration.C1orC2();
+        {
 
-            if (C1C2choice == '1')
+            int C1C2choice = Narration.C1orC2();
+            do
             {
-                GloomBeast();
-            }
-            if (C1C2choice == '2')
+                if ((C1C2choice != 1 && C1C2choice != 2 && C1C2choice != 0))
+                {
+                    Narration.InvalidInput();
+                    C1C2choice = Narration.C1orC2();
+                }
+
+
+            } while (C1C2choice != 1 && C1C2choice != 2 && C1C2choice != 0);
+
+            switch (C1C2choice)
             {
-                SpecialSword();
-            }
-            if (C1C2choice == '0')
-            {
-                Narration.ExitingCave();
-                Narration.VeldrosDialogue(); //PlayerName, Veldros.cs
-                Narration.PickRoute(); //PlayerName, Veldros.cs 
-            }
-            else
-            {
-                Narration.InvalidInput();
-                Cave1Or2();
+                case 1:
+                    GloomBeast();
+                    break;
+                case 2:
+                    SpecialSword();
+                    break;
+                case 0:
+                    Narration.ExitingCave();
+                    Narration.VeldrosDialogue();
+                    Narration.PickRoute();
+                    break;
+
             }
         }
-
         public static void GloomBeast()
         {
             int GloomBeastChoice = Narration.C1GloomBeast();
-            if (GloomBeastChoice == '1')
+            do
             {
-                //EnemyVsPlayer
+                if ((GloomBeastChoice != 1 && GloomBeastChoice != 2))
+                {
+                    Narration.InvalidInput();
+                    GloomBeastChoice = Narration.C1GloomBeast();
+                }
             }
-            if (GloomBeastChoice == '2')
+
+            while (GloomBeastChoice != 1 && GloomBeastChoice != 2);
+
+            switch (GloomBeastChoice)
             {
-                Cave1Or2();
-            }
-            else
-            {
-                Narration.InvalidInput();
-                Narration.C1GloomBeast();
+                case 1:
+                    // EnemyVsPlayer
+                    C3RetraceExit();
+                    break;
+                case 2:
+                    Cave1Or2();
+                    break;
+                default:
+                    break;
             }
         }
         public static void SpecialSword()
@@ -57,49 +74,89 @@ namespace MCNR
 
             while (guess != "LIGHT")
             {
-                Narration.SpecialSwordIncorrect(); 
+                Narration.SpecialSwordIncorrect();
                 guess = Narration.SpecialSwordGuess(); //new guess value
             }
             Narration.SpecialSwordCorrect();
-            char claim = Narration.SpecialSwordClaim();
+
+            char claim = Narration.SpecialSwordClaim(); //not sure if we wanna keep it as a char
+
+            while ((claim == 'P') || (claim == 'p'))
+            {
+                Narration.InvalidInput();
+                claim = Narration.SpecialSwordClaim();
+            }
 
             C3RetraceExit();
         }
-
         public static void C3RetraceExit()
         {
-           int choice = Narration.C3orRetraceorExit(); 
 
-            switch (choice) 
+            int choice = Narration.C3orRetraceorExit();
+            do
             {
-                case 1: 
-                    int gemStoneChoice = Narration.C3GemstoneTitan();
-                    if (gemStoneChoice == 1)
-                    {
-                        //EnemeyVsPlayer
-                    }
-                    if (gemStoneChoice == 2)
-                    {
-                        C3RetraceExit();
-                    }
-                    break;
-
-               case 2:
-                    Cave1Or2(); 
-                    Narration.RetracingSteps();
-                    break;
-
-               case 0:
-                    Narration.ExitingCave();
-                    break;
-
-               default:
+                if ((choice != 0 && choice != 1 && choice != 2))
+                {
                     Narration.InvalidInput();
+                    choice = Narration.C3orRetraceorExit();
+                }
+
+            }
+            while (choice != 0 && choice != 1 && choice != 2);
+
+            switch (choice)
+            {
+                case 1:
+                    GemStone();
+                    break;
+
+                case 2:
+                    Narration.RetracingSteps();
+                    Cave1Or2();
+                    break;
+
+                case 0:
+                    Narration.ExitingCave();
+                    //Korthak/Forest/Cave
+                    
+                    break;
+
+
+            }
+        }
+        public static void GemStone()
+        {
+            int GemStoneChoice = Narration.C3GemstoneTitan();
+            do
+            {
+                if ((GemStoneChoice != 1 && GemStoneChoice != 2))
+                {
+                    Narration.InvalidInput();
+                    GemStoneChoice = Narration.C3GemstoneTitan();
+                }
+            }
+            while (GemStoneChoice != 1 && GemStoneChoice != 2);
+
+            switch (GemStoneChoice)
+            {
+                case 1:
+                    // EnemyVsPlayer
+                    Narration.DeadEndCave();
+                    break;
+                case 2:
                     C3RetraceExit();
                     break;
+            }
+        }
 
+        public static void EscapeCave ()
+        {
+            Narration.DeadEndCave();
+
+            string answer = Narration.EscapeCaveRiddle();
             
 
         }
     }
 }
+
