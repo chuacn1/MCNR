5145b82ae2b06547b14f55b6d6681cf9195e1c3f
OLDILJ <OLDILJ1@op.ac.nz>
2025-03-07 13:42:22 +1300

    Created Consumable Class

---
+++ Consumable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCNR
+{
+    public class Consumable
+    {
+        string name {  get; set; }
+        int id {  get; set; }
+        public Consumable(string name, int id) 
+        {
+            this.name = name;
+            this.id = id;
+        }
+        //take a consumable and use its id and name to provide its effect
+        public void ConsumableSwitchTable(Consumable consumable) 
+        {
+            string ConsumName = consumable.name;
+            int IdToRemove = consumable.id;
+            ConsumName = ConsumName.ToLower();
+            //
+            switch(ConsumName)
+            {
+                case "strength":
+                    //Change Player base damage by 1
+                    ///Player.BaseDmg += 1
+                    break;
+                case "defense":
+                    //Increase Player base damage by 1
+                    ///Player.BaseDmg +=1
+                    break;
+                default:
+                    Console.WriteLine("I don't know how to use that");
+                    break;
+            }
+        }
+    }
+}
