154a63ab589da6249488ec7dcf8f9f66fd0a3b2a
unknown <KEVAD2SSTUDENT.op.ac.nz>
2025-03-14 15:52:51 +1300

    some error change

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Data.SqlTypes;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
+using mine;
 
 namespace MCNR
 {
@@ -12,7 +13,7 @@ namespace MCNR
         static int[] counts = new int[15];
         static int enemyHP;
         public static Player player;
-        public static Town town;
+        public static town town;
 
         public static Narration narration;
 
@@ -87,59 +88,9 @@ namespace MCNR
         }
         //**************************************************//
 
-        //*****WEAPON METHODS*****//
-        public class Weapon
-        {
+      
 
-            public string Name { get; set; }                        //able to access and modify its value
-            public int Damage { get; set; }                         //able to access and modify its value
-            public int UpgradeLevel { get; set; }                   //able to access and modify its value
-            public int UpgradeCost { get; set; }                    //able to access and modify its value
-            public int MaxUpgradeLevel { get; set; } = 5;           //able to access and modify its value
-
-            public Weapon(string name, int damage, int upgradeCost)
-            {
-                Name = name;
-
-                Damage = damage;
-
-                UpgradeLevel = 1; //sword starts at level 1
-
-                UpgradeCost = upgradeCost; //cost to upgrade
-
-            }
-
-        }
-
-        static void UpgradeWeapon(Weapon weapon)
-        {
-            if (weapon.UpgradeLevel < weapon.MaxUpgradeLevel)
-            {
-                if (ore > 0 && money >= weapon.UpgradeCost)
-                {
-                    //deduct iron and currency for upgrade
-                    ore--;
-                    money = weapon.UpgradeCost;
-
-                    //increase weapons damage and level
-                    weapon.UpgradeLevel++;
-                    weapon.Damage += 5;
-                    weapon.UpgradeCost += 10;
-
-                    Console.WriteLine($"Upgrade {weapon.Name} to level {weapon.UpgradeLevel}!");
-                    Console.WriteLine($"\nNew Damage: {weapon.Damage}, New Upgrade Cost: {weapon.UpgradeCost}");
-                    Console.WriteLine($"\nIron left: {ore}, Currency left: {money}");
-                }
-                else
-                {
-                    Console.WriteLine("Not enough iron or currency to upgrade weapon");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Maximum upgrade level reached for this weapon");
-            }
-        }
+        
         //**************************************************//
 
         //*****MAIN METHOD*****//
